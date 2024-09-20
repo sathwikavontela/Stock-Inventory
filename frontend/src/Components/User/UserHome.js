04f4ed65aaa5ee318cb7c9361b43934bb5a6ea19
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-09-20 14:15:23 +0530

    solved conflicts

--- frontend/src/Components/User/UserHome.js
+++ frontend/src/Components/User/UserHome.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserHeader from './UserHeader'
 import UserSidebar from './UserSidebar'
+import UserBody from './UserBody'
 
 const UserHome = () => {
   return (
@@ -8,7 +9,9 @@ const UserHome = () => {
       <UserHeader />
       <div className="pt-16 h-[100vh] flex">
         <UserSidebar />
-        <div className="p-4"> </div>
+        <div className="p-4">
+          <UserBody />
+        </div>
       </div>
     </div>
   )
