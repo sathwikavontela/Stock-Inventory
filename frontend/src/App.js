44aff1ec6e0450013b00bb0a1d9c533684d86af0
= <boyirirakesh.com>
2024-09-20 14:47:11 +0530

    solved-conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Components/Home/Home'
-<<<<<<< HEAD
 import LoginSignup from './Components/Login/LoginSignup';
 import RequestForm from './Components/Login/RequestForm';
 import About from './Components/About/About'
-=======
 import UserHome from './Components/User/UserHome'
 import Orders from './Components/User/Orders'
 import Reports from './Components/User/Reports'
->>>>>>> 859d4646d7351c2ad94d69e38bb7ff1f029b6a39
 const Applayout = () => {
   return (
     <div>
@@ -27,7 +24,6 @@ const appRouter = createBrowserRouter([
         element: <Home />,
       },
       {
-<<<<<<< HEAD
         path: '/login',
         element: <LoginSignup />,
       },
@@ -38,7 +34,6 @@ const appRouter = createBrowserRouter([
       {
         path: '/about-us',
         element: <About />,
-=======
         path: '/user-Home',
         element: <UserHome />,
       },
@@ -49,7 +44,6 @@ const appRouter = createBrowserRouter([
       {
         path: '/reports',
         element: <Reports />,
->>>>>>> 859d4646d7351c2ad94d69e38bb7ff1f029b6a39
       },
     ],
   },
